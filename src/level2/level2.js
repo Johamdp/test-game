517c5558851012a5566648aa06dbd3decb23f301
Johanna Yañez <johamdq25@gmail.com>
2022-07-29 10:36:04 -0300

    commit2

--- src/level2/level2.js
+++ src/level2/level2.js
@@ -3,92 +3,117 @@
 export function obtenerMayor(x, y) {
   // "x" e "y" son números enteros (int).
   // Tu código:
-
-  return;
+  return x>y ? x : y;
 }
 
 export function mayoriaDeEdad(edad) {
   // Tu código:
-
-  return;
+  return edad>=18 ? 'Allowed' : 'Not allowed';
 }
   
 export function conection(status) {
   //Recibimos un estado de conexión de un usuario representado por un valor numérico. 
   // Tu código:
-
-  return;
+  const values = ['Online','Away','Offline']
+  return values[status-1];
 }
 
 export function saludo(idioma) {  
-  // Tu código:
-  
-  return;
+  const saludos = {
+    aleman : 'Guten Tag!',
+    ingles : 'Hello!',
+    mandarin : 'Ni Hao!',
+    frances: 'Salut'
+  }
+  return saludos[idioma] || 'Hola!';
 }
 
 export function colors(color) {  
   //Usar el statement Switch.
   // Tu código:
- 
+  const validos = ['blue','red','green','orange']
+  return validos.indexOf(color)>=0 ? `This is ${color}` : 'Color not found' 
 }
 
 export function esDiezOCinco(numero) {
   // Tu código:
-
-  return;
+  return numero === 10 || numero === 5;
 }
 
 export function estaEnRango(numero) {  
   // Tu código:
-
-  return;
+  return numero > 20 && numero < 50;
 }
 
 export function esEntero(numero) {
   // Tu código:
-
-  return;
+  return numero % 1 === 0;
 }
 
 export function fizzBuzz(numero) {
   // Tu código:
- 
-  return;
+  if(numero % 15 == 0){
+    return 'fizzbuzz';
+  }else if (numero % 3 == 0){
+      return 'fizz';
+    }else if (numero % 5 == 0){
+       return 'buzz';
+    }else
+      return numero;
 }
 
 export function operadoresLogicos(num1, num2, num3) {
   //La función recibe tres números distintos. 
   // Tu código:
-  
-  return;
+  if(num1===0 || num2===0 || num3===0){
+    return "Error";
+  }else if(num > 0 || num2 > 0 || num3 > 0){
+    return "Hay negativos";
+  }else if(num1 > num2 && num1 > num3){
+    return "Numero 1 es mayor y positivo"
+  }else if(num3 > num1 && num3 > num2){
+    return num3 + 1;
+  }else{
+    return false;
+  } 
 }
 
 export function esPrimo(numero) {
-  /// Tu código:
+  /// Tu código: 
+  if (numero === 1 || numero === 0) {
+    return false;
+  }
 
-  return;  
+  for (let i = 2; i < numero; i++) {
+    if (numero % i === 0) {
+      return false;
+    }
+  }
+  return true;
 }
 
 export function esVerdadero(valor){
   // Tu código:
-
-  return;
+  return valor ? "Soy verdadero" : "Soy falso";
 }
 
 export function tablaDelSeis(){
   // Tu código:
-
-  return;
+  return [0 , 6, 12, 18, 24, 30, 36, 42, 48, 54, 60];
 }
 
 export function tieneTresDigitos(numero){
   // Tu código:
-
-  return ;  
+  return numero>99 && numero<1000;
 }
 
 export function doWhile(numero) {  
-  //Usar el bucle do ... while.
+  //Usar el bucle do ... while.  
+  let i = 0;
   
-  return;
+  do {
+    numero += 5;
+    i++;
+  } while (i < 8);
+  return numero;
 }
--- src/level3/level3.js
+++ src/level3/level3.js
@@ -1,141 +1,154 @@
 // No cambies los nombres de las funciones.
 
 export function devolverPrimerElemento(array) {
-    // Tu código:
-   
-    return;
+    // Tu código:   
+    return array[0];
   }
   
   
   export function devolverUltimoElemento(array) {
-    // Tu código:
-   
-    return;
+    // Tu código:   
+    return array[array.length-1];
   }
   
   
   export function obtenerLargoDelArray(array) {
-    // Tu código:
-   
-    return;
+    // Tu código:   
+    return array.length;
   }
   
   
   export function incrementarPorUno(array) {
     // Tu código:
-   
-    return;
+    return array.map(function (item) {
+      return item + 1;
+    });
   }
   
   
   export function agregarItemAlFinalDelArray(array, elemento) {
-    // Tu código:
-   
-    return;
+    // Tu código:   
+    return array.concat(elemento);
   }
   
   
   export function agregarItemAlComienzoDelArray(array, elemento) {
-    /// Tu código:
-   
-    return;
+    /// Tu código:   
+    return [elemento].concat(array);
   }
   
   
   export function dePalabrasAFrase(palabras) {
-    // Tu código:
-   
-    return;
+    // Tu código:   
+    return palabras.join(" ");
   }
   
   
   export function arrayContiene(array, elemento) {
-    // Tu código:
-   
-    return;
+    // Tu código:   
+    return array.includes(elemento);
   }
   
   
   export function agregarNumeros(numeros) {
-    // Tu código:
-   
-    return;
+    // Tu código:   
+    return numeros.reduce((x,y) => x+y, 0);
   }
   
   
   export function promedioResultadosTest(resultadosTest) {
-    // Tu código:
-   
-    return;
+    // Tu código:   
+    return resultadosTest.reduce((x,y)=> x+y, 0) / resultadosTest.length;
   }
   
   
   export function numeroMasGrande(numeros) {
-   // Tu código:
-   
-    return;
+   // Tu código:   
+   return numeros.reduce((x,y) => x>y?x:y);
   }
   
   
   export function multiplicarArgumentos() {
-    // Tu código:
-   
+    // Tu código:   
     return;
   }
   
   
   export function cuentoElementos(arreglo){
-   // Tu código:
-   
-   return;
+   // Tu código:   
+   let counter = 0;
+   arreglo.forEach((number) => (number > 18 ? counter++ : counter));
+   return counter;
   }
   
   
   export function diaDeLaSemana(numeroDeDia) {
-    // Tu código:
-   
-  
-    return;
+    // Tu código: 
+    let msg = 'Es dia de semana'
+    if (numeroDeDia == 1 || numeroDeDia == 7)
+      msg = 'Es fin de semana'
+    return msg;
   } 
   
   
   export function empiezaConNueve(n) {
-   // Tu código:
-      
-    return;  
+   // Tu código:      
+    return String(n)[0] == 9; 
   }
   
   
   export function todosIguales(arreglo) {
-   // Tu código:
-   
-    return;
+   // Tu código:   
+    return arreglo.every((item) => item === arreglo[0]);
   } 
   
   
   export function mesesDelAño(array) {  
-    // Tu código:
-   
-    return;
+    // Tu código:   
+    const newArray = ["Marzo", "Noviembre", "Enero"];
+    let contains =
+      array.includes(newArray[0]) &&
+      array.includes(newArray[1]) &&
+      array.includes(newArray[2]);
+    return contains ? newArray : "No se encontraron los meses pedidos";
   }
   
   
   export function mayorACien(array) {
-    // Tu código:
-  
-    return;
+    // Tu código:  
+    return array.filter(num=>num>100);
   }
   
   
   export function breakStatement(numero) {
-    // Tu código:
-  
-    return;
+    // Tu código:  
+    let arr = [];
+    let i = 0;
+    let sum = numero;
+
+    while(i<10 && i!=sum){
+      sum+=2;
+      arr.push(sum);
+      i++;
+    }
+
+    if(i==10){
+      return arr;
+    }else
+      return 'Se interrumpió la ejecución';
   }
-  
-  
+    
+    
   export function continueStatement(numero) {
-    // Tu código:
+    // Tu código:    
+  const array = [];
   
-    return;
+  for (let i = 0; i < 10; i++) {
+    if (i === 5) {
+      continue;
+    }
+    numero = numero + 2;
+    array.push(numero);
+  }
+  return array;
   }
